a19d76ec2dd9c3c662ee2c818a7dfdad4b9ba002
Dan Thompson <danthompson41@gmail.com>
2015-04-23 14:20:32 -0400

    Added toolbox to run code Changed TPR to correct implementation

--- dataTest.m
+++ dataTest.m
@@ -11,12 +11,13 @@ tpr_expected = zeros(1,numberOfWindows(2));
 tpr_actual = zeros(1,numberOfWindows(2));
 tpr_sigma_expected = zeros(1,numberOfWindows(2));
 tpr_sigma_real = zeros(1,numberOfWindows(2));
+tpr_ratio = zeros(1,numberOfWindows(2));
 se = zeros(1,numberOfWindows(2));
 rmssd = zeros(1,numberOfWindows(2));
  
  
  
-thr_tpr(1:numberOfWindows(2)) = 54;
+thr_tpr(1:numberOfWindows(2)) = .77;
 thr_se(1:numberOfWindows(2)) = .7;
 thr_rmssd(1:numberOfWindows(2)) = .1*mean(RRintervals);
 detected = zeros(1,28);
@@ -26,7 +27,8 @@ window = reshaped(:,i);
 [tpr_expected(i),tpr_actual(i),tpr_sigma_expected(i),tpr_sigma_real(i)] = turningPointRatio(window);
 se(i) = shannonEntropy(window);
 rmssd(i) = rootMeanSquareSuccessiveDifferences(window);
-if (tpr_actual(i) > thr_tpr(i)) & (se(i)> thr_se(i)) & (rmssd(i) > thr_rmssd)
+tpr_ratio(i) = tpr_actual(i) / tpr_expected(i);
+if (tpr_ratio(i) > thr_tpr(i)) & (se(i)> thr_se(i)) & (rmssd(i) > thr_rmssd)
 	detected(i) = 1;
 end
 end
@@ -38,7 +40,7 @@ x=1:numberOfWindows(2);
 figure
 subplot(5,1,2),plot(detected),title('Detected AFIB');
 subplot(5,1,1),plot(ekg),title('EKG');
-subplot(5,1,3),plot(x,tpr_actual,x,thr_tpr),title('Turning Point Ratio');
+subplot(5,1,3),plot(x,tpr_ratio,x,thr_tpr),title('Turning Point Ratio');
 subplot(5,1,4),plot(x,se,x,thr_se),title('Shannon Entropy');
 subplot(5,1,5),plot(x,rmssd,x,thr_rmssd),title('Root mean squared of Successive Differences');
 
